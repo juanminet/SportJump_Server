4b31b83c6dc95cd2b68cebafb8c0112d1160dc41
juanminet <juanminet@juanminet.(none)>
2012-10-22 23:48:27 +0200

    Add authorization and registration

---
+++ sportjumpPOM/sportjumpWEB/src/main/java/es/uma/sportjump/sjs/web/controller/AuthController.java
@@ -0,0 +1,81 @@
+package es.uma.sportjump.sjs.web.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.dao.SaltSource;
+import org.springframework.security.authentication.encoding.PasswordEncoder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import es.uma.sportjump.sjs.service.services.UserService;
+
+
+
+@Controller
+public class AuthController {
+
+	private static final String ROLE_USER = "ROLE_USER";
+
+	@Autowired
+	private UserDetailsManager userDetailsManager;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private SaltSource saltSource;
+	
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(value="/registration",method=RequestMethod.GET)
+	public String login() {
+		return "registration";
+	}
+	
+	@RequestMapping(value="/registration",method=RequestMethod.POST)
+	public String createUser(	@RequestParam("user")String username, 
+								@RequestParam("password") String password,
+								@RequestParam("name")String name, 
+								@RequestParam("surname") String surname,
+								@RequestParam("email") String email ) {
+		
+		
+		if (!userDetailsManager.userExists(username)) {
+			
+			addUserToAuthSystem(username,password);
+			
+			addUserToSportJump(username,name,surname,email);
+			
+		}
+		return "login";
+	}
+
+
+	private void addUserToAuthSystem(String username, String password) {
+		List<GrantedAuthority> authorites = new ArrayList<GrantedAuthority>();
+		authorites.add(new SimpleGrantedAuthority(ROLE_USER));
+		
+		
+		User user = new User(username, password, true, false, false, false, authorites);
+		User saltedUser = new User(username, passwordEncoder.encodePassword(password, saltSource.getSalt(user)), 
+				true, false, false, false, authorites);
+		
+		userDetailsManager.createUser(saltedUser);		
+	}
+	
+	
+	private void addUserToSportJump(String userName, String name, String surname, String email) {
+		userService.setNewCoach(name, userName, surname, email);		
+	}
+
+}
---
+++ sportjumpPOM/sportjumpWEB/src/main/webapp/WEB-INF/jsp/registration.jsp
@@ -0,0 +1,82 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="icon" href="${pageContext.request.contextPath}/resources/img/favicon.ico" type="image/ico" />
+<title>Sport Jump</title>
+</head>
+<body>
+
+<h1>REGISTRATION</h1>
+	
+		
+		<div class="registration" style="color:#FF9300;margin-left:-45px; margin-bottom:15px">	¿Todavía no tienes usuario en Sport Jump? Regístrate.</div>
+		<form action="registration" method="POST">
+			<table>
+				<tr>
+					<td>
+						<label for="user">Nombre usuario:</label>
+					</td>
+					<td>
+						<input style="width:250px" type="text" name="user" id="user" >
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<label for="password">Contraseña:</label>
+					</td>
+					<td>
+						<input style="width:250px" type="password" name="password" id="password" >
+					</td>
+				</tr>
+				
+				<tr>
+					<td>
+						<label for="repeatpassword">Repetir contraseña:</label>
+					</td>
+					<td>
+						<input style="width:250px" type="password" name="repeatpassword" id="repeatpassword" >
+					</td>
+				</tr>
+				
+				<tr>
+					<td>
+						<label for="user">Nombre:</label>
+					</td>
+					<td>
+						<input style="width:250px" type="text" name="name" id="name" >
+					</td>
+				</tr>
+				
+				<tr>
+					<td>
+						<label for="user">Apellidos:</label>
+					</td>
+					<td>
+						<input style="width:250px" type="text" name="surname" id="surname" >
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<label for="email">Email:</label>
+					</td>
+					<td>
+						<input style="width:250px" type="text" name="email" id="email" >
+					</td>
+				</tr>
+				
+				<tr>
+					<td>
+						
+					</td>
+					<td>
+						<input style="width:150px" type="submit" value="Registrarse"> 	
+					</td>
+				</tr>
+			</table>
+		 </form>
+	</div>
+</body>
+</html>
